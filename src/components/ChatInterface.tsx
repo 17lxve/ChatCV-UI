a755c365939b0bfecf123aa8eab34698f5bb5611
Timmy <kouadiobhegnino@gmail.com>
2024-05-13 03:53:18 +0000

    chat_view_0.5

--- src/components/ChatInterface.tsx
+++ src/components/ChatInterface.tsx
@@ -1,16 +1,27 @@
-import ViewBox from "./ViewBox";
+import { useState } from "react";
+import ViewBoxChat from "./ViewBoxChat";
 
 function ChatInterface() {
+    const [history, setHistory] = useState([{sender:"bot", content:"Bonjour! Comment-puis-je vous aider?"}])
+    const push = (message) => setHistory([...history, message])
   return (
-    <ViewBox>
+    <ViewBoxChat>
       <div>
         <h1 className="form-title">ChatCV</h1>
       </div>
-      <div>History</div>
-      <div>
-        <input type="text" />
+      <div className="msg-div">
+        {
+            Array.from(history).map(message => (
+                <div className={"message-" + message.sender}>
+                    <p>{message.content}</p>
+                </div>
+            ))
+        }
       </div>
-    </ViewBox>
+      <div>
+        <input type="text" className="chat-bar" id="chatbar" onKeyUp={e => e.key === "Enter" ? push({content:e.target.value, sender:"user"}):console.log(history)}/>
+      </div>
+    </ViewBoxChat>
   );
 }
 
